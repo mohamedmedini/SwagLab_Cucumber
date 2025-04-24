7c3a8ea8862fcb7070102519d857add1856a8cdf
Mohamed Medini <145905619+mohamedmedini@users.noreply.github.com>
2025-04-24 13:20:39 +0100

    Update DriverManager.java

--- SauceDemoPOM/src/test/java/Hooks/DriverManager.java
+++ SauceDemoPOM/src/test/java/Hooks/DriverManager.java
@@ -5,6 +5,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.chrome.ChromeOptions;
+import java.io.File;
 
 public class DriverManager {
 	
