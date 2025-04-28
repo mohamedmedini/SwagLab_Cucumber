cbdd392d21c8cef652d6e0d2896ad2c97f4540ed
Mohamed Medini <145905619+mohamedmedini@users.noreply.github.com>
2025-04-28 16:45:56 +0100

    Update DriverManager.java

--- SauceDemoPOM/src/test/java/Hooks/DriverManager.java
+++ SauceDemoPOM/src/test/java/Hooks/DriverManager.java
@@ -11,40 +11,40 @@ public class DriverManager {
 	
 public static WebDriver driver;
 	
-	    public static WebDriver GetDriver() {
-		WebDriverManager.chromedriver().setup();
-		driver=new ChromeDriver();
-		driver.manage().window().maximize();
-		driver.manage().timeouts().implicitlyWait(10,TimeUnit.SECONDS);
-		return driver;			
-	}
-}
+// 	    public static WebDriver GetDriver() {
+// 		WebDriverManager.chromedriver().setup();
+// 		driver=new ChromeDriver();
+// 		driver.manage().window().maximize();
+// 		driver.manage().timeouts().implicitlyWait(10,TimeUnit.SECONDS);
+// 		return driver;			
+// 	}
+// }
 
 // input chrome options to run github actions 
 
-// public static WebDriver driver;
-// //
-// public static WebDriver GetDriver() {
-//    if (driver == null) {
-//        WebDriverManager.chromedriver().setup();
-//        ChromeOptions options = new ChromeOptions();
-//        options.addArguments("--headless");
-//        options.addArguments("--disable-gpu");
-//        options.addArguments("--no-sandbox");
-//        options.addArguments("--disable-dev-shm-usage");
-//        options.addArguments("--remote-allow-origins=*");
+public static WebDriver driver;
+//
+public static WebDriver GetDriver() {
+   if (driver == null) {
+       WebDriverManager.chromedriver().setup();
+       ChromeOptions options = new ChromeOptions();
+       options.addArguments("--headless");
+       options.addArguments("--disable-gpu");
+       options.addArguments("--no-sandbox");
+       options.addArguments("--disable-dev-shm-usage");
+       options.addArguments("--remote-allow-origins=*");
 
-//         //Generate a unique user-data-dir for each test run
-//        String uniqueUserDataDir = System.getProperty("java.io.tmpdir") + "/chrome-profile-" + System.nanoTime();
-//        new File(uniqueUserDataDir).mkdirs();  // make sure it exists
-//        options.addArguments("--user-data-dir=" + uniqueUserDataDir);
+        //Generate a unique user-data-dir for each test run
+       String uniqueUserDataDir = System.getProperty("java.io.tmpdir") + "/chrome-profile-" + System.nanoTime();
+       new File(uniqueUserDataDir).mkdirs();  // make sure it exists
+       options.addArguments("--user-data-dir=" + uniqueUserDataDir);
 
-//        driver = new ChromeDriver(options);
-//        driver.manage().window().maximize();
-//        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-//    }
+       driver = new ChromeDriver(options);
+       driver.manage().window().maximize();
+       driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+   }
 
-//    return driver;
-//  }
+   return driver;
+ }
 
 	
